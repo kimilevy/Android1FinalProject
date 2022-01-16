27b3348cd171e03b3a102a6fc31d2e4cc9191d01
kim kim <kimlevy06@gmail.com>
2022-01-16 16:19:47 +0200

    when play button pressed-> move to main fragment page

--- app/src/main/java/com/hit/android1/finalproject/ui/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/hit/android1/finalproject/ui/welcome/WelcomeFragment.kt
@@ -1,11 +1,15 @@
 package com.hit.android1.finalproject.ui.welcome
 
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.hit.android1.finalproject.R
+import com.hit.android1.finalproject.app.AppFragment
 import com.hit.android1.finalproject.app.AppFragmentWithModel
 import com.hit.android1.finalproject.app.Globals
 import com.hit.android1.finalproject.databinding.MainFragmentBinding
+import com.hit.android1.finalproject.databinding.WelcomeFragmentBinding
 import com.hit.android1.finalproject.ui.main.InventoryAdapter
 import com.hit.android1.finalproject.ui.main.MainViewModel
 import kotlinx.coroutines.Dispatchers
@@ -13,26 +17,15 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class WelcomeFragment : AppFragmentWithModel<MainFragmentBinding, MainViewModel>(MainViewModel::class.java) {
-    override fun inflate() = MainFragmentBinding.inflate(layoutInflater)
+class WelcomeFragment : AppFragment<WelcomeFragmentBinding>() {
+    override fun inflate() = WelcomeFragmentBinding.inflate(layoutInflater)
 
     override fun runOnCreateView(view: View) {
-        viewModel.cool = "UNCOOL ANYMORE 🎨"
-        initializeViewModel()
+        binding.playButtonGame.setOnClickListener {
+            findNavController().navigate(R.id.mainFragment)
+        }
+
     }
 
-    private suspend fun initializeRecycler() {
-        withContext(Dispatchers.Main) {
-            binding.itemInventoryRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2, RecyclerView.HORIZONTAL, false)
-            binding.itemInventoryRecyclerView.adapter = InventoryAdapter(viewModel.items!!)
-        }
-    }
 
-    private fun initializeViewModel() {
-        GlobalScope.launch(Dispatchers.IO) {
-            viewModel.items = Globals.dao.getItems()
-            viewModel.unlockedItems = Globals.dao.getUnlockedItems()
-            initializeRecycler()
-        }
-    }
 }
